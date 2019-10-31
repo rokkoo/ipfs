85df48253e96625a4db1f1b974899819b293220b
rokkoo <alfonsolasaguirre@gmail.com>
2019-10-31 14:11:13 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import * as ipfsClient from 'ipfs-http-client';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
+let infuraIpfs = ipfsClient({
+   host: 'ipfs.infura.io',
+   port: '5001',
+   protocol: 'https'
+});
+const ipfsEndpoint = 'https://ipfs.infura.io:5001/api/v0';
+
+const bufferFile = content => Buffer.from(JSON.stringify(content));
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [hash, setHash] = useState(null);
+
+   const handleSubmit = e => {};
+   const captureFile = async e => {
+      e.stopPropagation();
+      e.preventDefault();
+      await ipfsAdd(e.target.files);
+   };
+
+   const ipfsAdd = async content => {
+      const file = bufferFile(content);
+
+      try {
+         const [result] = await infuraIpfs.add(file);
+         console.log(result.hash);
+         setHash(result.hash);
+      } catch (err) {
+         console.error('Error pinning file to IPFS', err);
+      }
+   };
+
+   const ipfsGet = async hash => {
+      const endpoint = `${ipfsEndpoint}/cat?arg=${hash}`;
+      try {
+         const { data } = await axios.get(endpoint);
+         return data;
+      } catch (err) {
+         console.error('Error getting data from IPFS', err);
+      }
+   };
+
+   return (
+      <div className="App">
+         <form id="captureMedia" onSubmit={handleSubmit}>
+            <input type="file" onChange={captureFile} />
+            <br />
+         </form>
+      </div>
+   );
 }
 
 export default App;
